70be4a792007ff44edd66d3f97c9f7258437ee4c
Dayz221 <rutuchenal37@gmail.com>
2024-09-12 20:05:25 +0300

    release

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ import { TELEGRAM_BOT_PASSWORD } from "./config.js"
 import Group from "./models/group.js"
 import serverless from "serverless-http"
 
-const PORT = process.env.PORT ?? 8000
-const ADDRESS = "localhost"
+const PORT = process.env.PORT || 8000
 
 const app = express()
 
@@ -24,28 +23,26 @@ app.use("/api/files", fileRouter)
 
 app.get('/api', (req, res) => res.status(200).send({ message: "homework site and telegram-bot api" }))
 
-if (process.env.NODE_ENV === "dev") {
-    app.listen(PORT, async (err) => {
-        if (err) return console.log(color.red(err))
-    
-        await mongoose
-            .connect("mongodb+srv://dayz221:qwerfvbhu123@app.7iifv.mongodb.net/?retryWrites=true&w=majority&appName=app")
-            .then(() => { console.log(color.green(`MongoDB attached!`)) })
-            .catch((err) => { console.log(color.red(err)) })
-    
-        try {
-            const botAdmin = await User.findOne({ login: "TelegramBot" })
-            if (!botAdmin) {
-                const groupId = await Group.findOne({ name: "ИУ7-16Б" })
-                const bot = new User({ login: "TelegramBot", password: bcrypt.hashSync(TELEGRAM_BOT_PASSWORD, 8), groupId: groupId._id, telegramId: 0, permissions: 3 })
-                await bot.save()
-            }
-        } catch (e) {
-            console.log(e)
+app.listen(PORT, async (err) => {
+    if (err) return console.log(color.red(err))
+
+    await mongoose
+        .connect("mongodb+srv://dayz221:qwerfvbhu123@app.7iifv.mongodb.net/?retryWrites=true&w=majority&appName=app")
+        .then(() => { console.log(color.green(`MongoDB attached!`)) })
+        .catch((err) => { console.log(color.red(err)) })
+
+    try {
+        const botAdmin = await User.findOne({ login: "TelegramBot" })
+        if (!botAdmin) {
+            const groupId = await Group.findOne({ name: "ИУ7-16Б" })
+            const bot = new User({ login: "TelegramBot", password: bcrypt.hashSync(TELEGRAM_BOT_PASSWORD, 8), groupId: groupId._id, telegramId: 0, permissions: 3 })
+            await bot.save()
         }
-    
-        console.log(color.green(`Server started on http://${ADDRESS}:${PORT}`))
-    })    
-}
+    } catch (e) {
+        console.log(e)
+    }
+
+    console.log(color.green(`Server started on http://localhost:${PORT}`))
+})    
 
 export const handler = serverless(app);
--- models/import requests #библиотека для отправки.py
+++
@@ -1,7 +0,0 @@
-import requests #библиотека для отправки запросов
-data = { "login": "TelegramBot", "password": "1234" }
-headers = {  "Authorization": "Bearer <token>" }
-files = { "file": open(<path>, "rb" ) }
-response = requests.post("http://localhost:8000/api/auth/login", json=data, headers=headers, files=files)
-print(response.status_code)
-print(response.json())
